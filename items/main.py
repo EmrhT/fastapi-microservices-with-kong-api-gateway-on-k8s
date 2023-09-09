4dcf5e6ce0ae96cf2a6ef9bb07701023803a54f2
emrht <emrhtfn@gmail.com>
2023-09-09 23:27:10 +0300

    items microservice added

---
+++ items/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, HTTPException, Depends, Response
+from typing import List
+from sqlalchemy.orm import Session
+from models import Item, Item_Pydantic, get_db
+
+
+app = FastAPI()
+
+# Create a GET endpoint to retrieve all items
+@app.get("/items/", response_model=List[Item_Pydantic])
+async def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    items = db.query(Item).offset(skip).limit(limit).all()
+    return items
+
+
+# Create a POST endpoint to create a new item
+@app.post("/items/")
+async def create_item(item: Item_Pydantic, db: Session = Depends(get_db)):
+    db_item = Item(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+# Create an UPDATE endpoint to update an existing item by its ID
+@app.put("/items/{item_id}/", response_model=Item_Pydantic)
+async def update_item(item_id: int, updated_item: Item_Pydantic, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    print(db_item.__dict__)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    for key, value in updated_item.dict().items():
+        if key == "id":
+            setattr(db_item, key, item_id)
+            continue
+        setattr(db_item, key, value)
+
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+# Delete operation
+@app.delete("/items/{item_id}", response_model=Item_Pydantic)
+async def delete_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    db.delete(item)
+    db.commit()
+    return Response(status_code=204)
+
---
+++ items/models.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from typing import Optional
+from sqlalchemy import create_engine, Column, Integer, String, delete
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Create an SQLite in-memory database
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Define an SQLAlchemy model for the database
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, index=True)
+    description = Column(String)
+
+class Item_Pydantic(BaseModel):
+    id: Optional[int]
+    name: str
+    description: str
+    class Config:
+        orm_mode = True
+
+Base.metadata.create_all(bind=engine)
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
